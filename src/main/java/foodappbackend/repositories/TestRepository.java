9c5125ba1116ff42aed6634d05e94dc0170ee279
Joris Theunissen <joristheunissen@telenet.be>
2019-02-15 12:01:35 +0100

    Remove testing classes

--- src/main/java/foodappbackend/repositories/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package foodappbackend.repositories;
-
-import foodappbackend.model.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestRepository extends CrudRepository<TestEntity, Long> {
-    Iterable<TestEntity> findAll();
-}
