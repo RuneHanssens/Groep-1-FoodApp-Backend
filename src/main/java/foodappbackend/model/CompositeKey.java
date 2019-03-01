14933621b9c90d521ca05bcd35eaeab4400b9c97
Joris Theunissen <joristheunissen@telenet.be>
2019-03-01 10:56:05 +0100

    Attempt at solving duplicate day entries v26

--- src/main/java/foodappbackend/model/CompositeKey.java
+++ src/main/java/foodappbackend/model/CompositeKey.java
@@ -1,9 +1,11 @@
 package foodappbackend.model;
 
+import foodappbackend.user.ApplicationUser;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
 public class CompositeKey implements Serializable {
     public LocalDate date;
-    public String username;
+    public ApplicationUser user;
 }
