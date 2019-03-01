2d0f8c33b79f1255c2938af90edcdffa516c5f24
Joris Theunissen <joristheunissen@telenet.be>
2019-03-01 19:49:03 +0100

    Fourth attempt at an error result

--- src/main/java/foodappbackend/controllers/ErrorController.java
+++ src/main/java/foodappbackend/controllers/ErrorController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
     @RequestMapping(value = "/error", method = RequestMethod.GET)
-    public String renderErrorPage(HttpServletRequest httpRequest) {
+    public String handleError(HttpServletRequest httpRequest) {
         switch (getErrorCode(httpRequest)) {
             case 400:
                 return "Bad Request";
@@ -27,5 +27,10 @@ public class ErrorController {
 
     private int getErrorCode(HttpServletRequest httpRequest) {
         return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
